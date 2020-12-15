d8f436ab2b10418f79d2834b1d4bb9508d10be49
Deniz Can TOŞUR <deniz_07ant@outlook.com>
2020-12-15 18:03:00 +0300

    first commit

---
+++ Stored_deneme1/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Stored_deneme1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Ekle = new System.Windows.Forms.Button();
+            this.Silme = new System.Windows.Forms.Button();
+            this.Güncelleme = new System.Windows.Forms.Button();
+            this.Temizle = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.Göster = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mno:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tc:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ad:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(70, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Soyad:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(341, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Telefon:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(347, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Email:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(341, 104);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Reztarih:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(347, 143);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 17);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Kisisayı:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 206);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1223, 257);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // Ekle
+            // 
+            this.Ekle.Location = new System.Drawing.Point(669, 44);
+            this.Ekle.Name = "Ekle";
+            this.Ekle.Size = new System.Drawing.Size(147, 42);
+            this.Ekle.TabIndex = 9;
+            this.Ekle.Text = "Ekle";
+            this.Ekle.UseVisualStyleBackColor = true;
+            this.Ekle.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Silme
+            // 
+            this.Silme.Location = new System.Drawing.Point(879, 44);
+            this.Silme.Name = "Silme";
+            this.Silme.Size = new System.Drawing.Size(147, 42);
+            this.Silme.TabIndex = 10;
+            this.Silme.Text = "Silme";
+            this.Silme.UseVisualStyleBackColor = true;
+            this.Silme.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Güncelleme
+            // 
+            this.Güncelleme.Location = new System.Drawing.Point(1045, 45);
+            this.Güncelleme.Name = "Güncelleme";
+            this.Güncelleme.Size = new System.Drawing.Size(147, 42);
+            this.Güncelleme.TabIndex = 11;
+            this.Güncelleme.Text = "Güncelleme";
+            this.Güncelleme.UseVisualStyleBackColor = true;
+            this.Güncelleme.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Temizle
+            // 
+            this.Temizle.Location = new System.Drawing.Point(1045, 138);
+            this.Temizle.Name = "Temizle";
+            this.Temizle.Size = new System.Drawing.Size(147, 42);
+            this.Temizle.TabIndex = 12;
+            this.Temizle.Text = "Temizle";
+            this.Temizle.UseVisualStyleBackColor = true;
+            this.Temizle.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(124, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(142, 22);
+            this.textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(124, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(142, 22);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(124, 101);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(142, 22);
+            this.textBox3.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(124, 138);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(142, 22);
+            this.textBox4.TabIndex = 16;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(453, 27);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(153, 22);
+            this.textBox5.TabIndex = 17;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(453, 70);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(153, 22);
+            this.textBox6.TabIndex = 18;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(453, 138);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(153, 22);
+            this.textBox7.TabIndex = 19;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(453, 104);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(145, 22);
+            this.dateTimePicker1.TabIndex = 20;
+            // 
+            // Göster
+            // 
+            this.Göster.Location = new System.Drawing.Point(681, 141);
+            this.Göster.Name = "Göster";
+            this.Göster.Size = new System.Drawing.Size(118, 37);
+            this.Göster.TabIndex = 21;
+            this.Göster.Text = "Göster";
+            this.Göster.UseVisualStyleBackColor = true;
+            this.Göster.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1232, 481);
+            this.Controls.Add(this.Göster);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Temizle);
+            this.Controls.Add(this.Güncelleme);
+            this.Controls.Add(this.Silme);
+            this.Controls.Add(this.Ekle);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Ekle;
+        private System.Windows.Forms.Button Silme;
+        private System.Windows.Forms.Button Güncelleme;
+        private System.Windows.Forms.Button Temizle;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button Göster;
+    }
+}
+
---
+++ Stored_deneme1/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient; //Sql komutlarını ve parametrelerini kullanmamızı sağlar.
+namespace Stored_deneme1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglantı = new SqlConnection("Data Source=MSI\\SQLEXPRESS02;Initial Catalog=vt;Integrated Security=True;");
+        //New connection sınıfından yeni bir nesne üretiyoruz.Bu nesne SQL Server veritabanına olan bağlantıyı temsil ediyor//
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            baglantı.Open(); //Baglantımızı açtıyoruz//
+            SqlCommand komut = new SqlCommand("göster",baglantı); //SqlCommand komutu ile 'göster' stored procedures e bağlandık
+            komut.CommandType = CommandType.StoredProcedure; //Kullanıcağımız sql komutunu Stored procedure olarak belirledik.
+            DataTable dt = new DataTable(); //data table sınıfından yeni bir nesne türetiyoruz.Tabloları listelemek için.
+            dt.Load(komut.ExecuteReader());  //Veri kaynağımızdan verileri çekiyoruz.
+            dataGridView1.DataSource = dt;  //tabloları listeleyip dataGridView'e bağlıyoruz.
+            baglantı.Close(); //Bağlantımızı kapattıyoruz.
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglantı.Open(); //Bağlantımızı açıyoruz.//
+            SqlCommand komut = new SqlCommand("Ekle", baglantı); //SqlCommand komutu ile 'ekle' stored procedures e bağlandık.
+            komut.CommandType = CommandType.StoredProcedure;  //Kullanıcağımız sql komutunu Stored procedure olarak belirledik.
+            komut.Parameters.AddWithValue("@Mno",textBox1.Text); 
+            komut.Parameters.AddWithValue("@Tc",textBox2.Text);
+            komut.Parameters.AddWithValue("@Ad",textBox3.Text); //Komut parametremiz ile textboxlara yazdığımız değerleri ekliyoruz.
+            komut.Parameters.AddWithValue("@Soyad",textBox4.Text);
+            komut.Parameters.AddWithValue("@Telefon",textBox5.Text);
+            komut.Parameters.AddWithValue("@Email",textBox6.Text);
+            komut.Parameters.AddWithValue("@Reztarih",dateTimePicker1.Value);
+            komut.Parameters.AddWithValue("@Kisisayı",textBox7.Text);
+            komut.ExecuteNonQuery(); //Veri tabanı içerisinde değişiklik yapacağımız komut işlemi burda gerçekleşiyor.
+            baglantı.Close(); //Bağlantımızı kapattıyoruz.//
+            MessageBox.Show("Kayıt Ekleme İşlemi Başarılı!!"); //Mesaj kutucuğu gönderiyoruz.
+
+
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();  //dataGridView'de tıkladığımız verinin bilgileri TextBoxlara gönderiyoruz.
+            textBox3.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            textBox5.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            textBox6.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+            dateTimePicker1.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
+            textBox7.Text = dataGridView1.CurrentRow.Cells[7].Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            baglantı.Open(); //Bağlantımızı açıyoruz.//
+            SqlCommand komut = new SqlCommand("silme", baglantı);//SqlCommand komutu ile 'silme' stored procedures e bağlandık
+            komut.CommandType = CommandType.StoredProcedure; //Kullanıcağımız sql komutunu Stored procedure olarak belirledik.
+            komut.Parameters.AddWithValue("Mno", textBox1.Text); //Mno ya göre kişiyi listeden silme komutu sağladık.
+            komut.ExecuteNonQuery(); //Veri tabanı içerisinde değişiklik yapacağımız komut işlemi burda gerçekleşiyor.
+            baglantı.Close(); //Bağlantımızı kapattıyoruz.//
+            MessageBox.Show("Kayıt Silme İşlemi Başarılı!!"); //Mesaj kutucuğu gönderiyoruz.
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            baglantı.Open(); //Bağlantımızı açıyoruz.//
+            SqlCommand komut = new SqlCommand("güncelleme", baglantı); //SqlCommand komutu ile 'güncelleme' stored procedures e bağlandık
+            komut.CommandType = CommandType.StoredProcedure; //Kullanıcağımız sql komutunu Stored procedure olarak belirledik.
+            komut.Parameters.AddWithValue("@Mno", textBox1.Text);
+            komut.Parameters.AddWithValue("@Tc", textBox2.Text); //Komut parametremiz ile textboxlara güncelleyeceğimiz değerleri ekliyoruz.
+            komut.Parameters.AddWithValue("@Ad", textBox3.Text);
+            komut.Parameters.AddWithValue("@Soyad", textBox4.Text);
+            komut.Parameters.AddWithValue("@Telefon",textBox5.Text);
+            komut.Parameters.AddWithValue("@Email", textBox6.Text);
+            komut.Parameters.AddWithValue("@Kisisayı", textBox7.Text);
+            komut.Parameters.AddWithValue("@Reztarih", dateTimePicker1.Value);
+            komut.ExecuteNonQuery(); //Veri tabanı içerisinde değişiklik yapacağımız komut işlemi burda gerçekleşiyor.
+            baglantı.Close(); //Bağlantımızı kapattıyoruz.//
+            MessageBox.Show("Kayıt Güncelleme İşlemi Başarılı!!"); //Mesaj kutucuğu gönderiyoruz.
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear(); //Textboxların içindeki veriyi temizliyor.
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();            
+            textBox1.Focus();
+        }
+    }
+}
